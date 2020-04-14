3b74e561f2d5d8821fe8dc7795309c2d6f07f724
praveen b <pvballi@outlook.com>
2020-04-14 22:09:54 +0530

    Add project files.

---
+++ hastable/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Threading;
+
+namespace hastable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hashtable ht = new Hashtable();
+            ht.Add("Name", "praveen");
+            ht.Add("Job","Developer" );
+            ht.Add("city", "Hyd");
+            foreach(object key in ht.Keys)
+                Console.WriteLine(key+":"+ht[key]);
+           // Console.WriteLine(ht["Name"]);
+        }
+    }
+}
